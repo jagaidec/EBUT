e50f1d6a27982b54f0bb07ba676f84f6b815bdb6
jagaidec <jagaidec@htwg-konstanz.de>
2014-12-02 11:05:51 +0100

    Added DownloadAction.java

---
+++ EclipsProject/WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/DownloadAction.java
@@ -0,0 +1,38 @@
+package de.htwg_konstanz.ebus.wholesaler.action;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
+import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants.ExportFormat;
+import de.htwg_konstanz.ebus.wholesaler.main.Exporter;
+
+public class DownloadAction implements IAction {
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response, ArrayList<String> errorList) {
+		String page = "export.jsp";
+		LoginBean login = (LoginBean) request.getSession().getAttribute(
+				Constants.PARAM_LOGIN_BEAN);
+		if (login.isLoggedIn() && login != null) {
+			
+		} else {
+			errorList.add("Please log in!");
+			page = "login.jsp";
+		}
+
+		return page;
+	}
+
+	@Override
+	public boolean accepts(String actionName) {
+		return actionName.equalsIgnoreCase(Constants.EXPORT);
+	}
+
+}
