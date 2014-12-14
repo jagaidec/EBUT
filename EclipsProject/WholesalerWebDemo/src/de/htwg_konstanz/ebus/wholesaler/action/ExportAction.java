7370b16ed429a86e6347cc6b20294d99a54225ce
jagaidec <jagaidec@htwg-konstanz.de>
2014-12-14 11:44:15 +0100

    Export Basti

--- EclipsProject/WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ExportAction.java
+++ EclipsProject/WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/action/ExportAction.java
@@ -9,37 +9,47 @@ import javax.servlet.http.HttpServletResponse;
 import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
 import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
 import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
-import de.htwg_konstanz.ebus.wholesaler.demo.util.ExportFormat;
-import de.htwg_konstanz.ebus.wholesaler.main.Exporter;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants.ExportFormat;
+import de.htwg_konstanz.ebus.wholesaler.main.ExportHandler;
 
 public class ExportAction implements IAction {
 
 	@Override
-	public String execute(HttpServletRequest request,
-			HttpServletResponse response, ArrayList<String> errorList) {
-		String page = "export.jsp";
-		LoginBean login = (LoginBean) request.getSession().getAttribute(
-				Constants.PARAM_LOGIN_BEAN);
-		if (login.isLoggedIn() && login != null) {
-			String description = request.getParameter(Constants.SEARCH_STRING);
-			ExportFormat exportFormat = ExportFormat.getExportFormat(request.getParameter(Constants.FILE_TYPE));
-			
-			Exporter exporter = new Exporter(description, exportFormat);
-			File exportFile = exporter.export();
-			
-			request.getSession(true).setAttribute("exportfile", exportFile);
-			
-		} else {
-			errorList.add("Please log in!");
-			page = "login.jsp";
-		}
+	public String execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> errorList) {
+		String nextPage = "export.jsp";
 
-		return page;
+		// ----- Check if User is logged in correctly + check if user is allowed to use action ----- //
+		
+		//Gets the LoginBean from the Session
+		LoginBean loginBean = (LoginBean) request.getSession().getAttribute(Constants.PARAM_LOGIN_BEAN);
+		
+		//Check if user is logged in correctly
+		if (loginBean.isLoggedIn() && loginBean != null) {
+			//Used for export with search Words
+			String shortDescription = request.getParameter(Constants.PARAM_SEARCH_STRING);
+			System.out.println(shortDescription + " ExportAction Zeile 30");
+			//Get fileType
+			ExportFormat fileType = Constants.ExportFormat.getExportFormat(request.getParameter(Constants.PARAM_FILE_TYPE));
+
+			//Create ExportHandler and start Export
+			ExportHandler exportHandler = new ExportHandler(shortDescription, fileType);
+			File exportFile = exportHandler.export();
+
+			request.getSession(true).setAttribute("exportfile", exportFile);
+	
+		} else {		
+			//if user is not logged in correctly redirect to loginPage
+			errorList.add("Please log in first!");
+			nextPage = "login.jsp";
+		}
+		
+		//if User is logged in correct he is allowed to export something
+		return nextPage;
 	}
 
 	@Override
 	public boolean accepts(String actionName) {
-		return actionName.equalsIgnoreCase(Constants.EXPORT);
+		return actionName.equalsIgnoreCase(Constants.ACTION_DOWNLOAD_CATALOG);
 	}
 
 }
