d20a395dc902c417c6433867087e4eb0e6319dde
faambros <f.ambrosi@web.de>
2015-01-07 12:39:08 +0100

    add project

---
+++ EclipsProject/WholesalerWebDemo/src/de/htwg_konstanz/ebus/wholesaler/exceptions/DocumentNotValidException.java
@@ -0,0 +1,9 @@
+package de.htwg_konstanz.ebus.wholesaler.exceptions;
+
+public class DocumentNotValidException extends Exception {
+
+	public DocumentNotValidException(String msg) {
+		super(msg);
+	}
+
+}
