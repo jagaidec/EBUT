acf5c50e369c6cddcee5561cc483bd1328e7080a
faambros <f.ambrosi@web.de>
2015-01-07 12:36:43 +0100

    add

--- EclipsProject/WholesalerWebDemo/install/createdb.bat
+++
@@ -1,3 +0,0 @@
-mysqladmin --host=localhost --user=%2 --password=%3 drop %1 -f
-mysqladmin --host=localhost --user=%2 --password=%3 create %1
-mysql --host=localhost --user=%2 --password=%3 --database=%1 < ebus.sql
